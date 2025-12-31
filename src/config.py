400b917ea2f393e542503ac13290bda68a50fdd8
unstableme <unstableme02@gmail.com>
2025-12-31 20:42:33 +0545

    config updated

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import yaml
 from pathlib import Path
 
-PARAMS_PATH = Path(__file__).parent[1] / 'params.yaml'
+PARAMS_PATH = Path(__file__).parents[1] / 'params.yaml'
 def load_params():
     """Load parameters from params.yaml file."""
     with open(PARAMS_PATH, 'r') as file:
